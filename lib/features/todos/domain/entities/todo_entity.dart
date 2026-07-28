ad3c94595abb9ea7fdb562cf0cd928311a2749ac
Ade Ripaldi <ripaldinuralim@gmail.com>
2026-07-28 13:17:06 +0700

    Step 1: Buat Entity Post

---
+++ lib/features/todos/domain/entities/todo_entity.dart
@@ -0,0 +1,15 @@
+class TodoEntity {
+  const TodoEntity({
+    required this.id,
+    required this.userId,
+    required this.title,
+    required this.dueOn,
+    required this.status,
+  });
+
+  final int id;
+  final int userId;
+  final String title;
+  final DateTime dueOn;
+  final bool status;
+}
