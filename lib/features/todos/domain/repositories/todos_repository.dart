d12b8b08084b374a335bef0ebb0a94a170839ac3
Ade Ripaldi <ripaldinuralim@gmail.com>
2026-07-28 17:56:57 +0700

    Step 3: Tambahkan Method di Repository Contract

--- lib/features/todos/domain/repositories/todos_repository.dart
+++ lib/features/todos/domain/repositories/todos_repository.dart
@@ -1,7 +1,10 @@
 import '../../../../core/utils/app_result.dart';
 import '../entities/todo_entity.dart';
+import '../entities/todo_request.dart';
 
 abstract class TodosRepository {
   Future<AppResult<List<TodoEntity>>> getTodos();
   Future<AppResult<TodoEntity>> getTodo(int id);
+  Future<AppResult<TodoEntity>> createUser(int userId, TodoRequest request);
+
 }
