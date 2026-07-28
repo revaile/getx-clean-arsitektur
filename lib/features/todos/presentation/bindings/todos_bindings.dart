d8a02c24e17a6d0682ddef1256c3548959750fea
Ade Ripaldi <ripaldinuralim@gmail.com>
2026-07-28 20:27:18 +0700

    Step 8: Update Todos Binding

--- lib/features/todos/presentation/bindings/todos_bindings.dart
+++ lib/features/todos/presentation/bindings/todos_bindings.dart
@@ -5,6 +5,7 @@ import '../../../../core/network/api_client.dart';
 import '../../data/datasources/todos_remote_data_source.dart';
 import '../../data/repositories/todos_repository_impl.dart';
 import '../../domain/repositories/todos_repository.dart';
+import '../../domain/usecases/create_todo_usecase.dart';
 import '../../domain/usecases/get_todos_usecase.dart';
 import '../controllers/todos_controller.dart';
 
@@ -19,10 +20,14 @@ class TodosBinding extends Bindings {
     );
     Get.lazyPut(() => GetTodosUseCase(Get.find<TodosRepository>()));
     Get.lazyPut(() => GetTodoUseCase(Get.find<TodosRepository>()));
+    Get.lazyPut(() => CreateTodoUseCase(Get.find<TodosRepository>()));
+
     Get.lazyPut(
       () => TodosController(
         Get.find<GetTodosUseCase>(),
-        Get.find<GetTodoUseCase>()
+        Get.find<GetTodoUseCase>(),
+        Get.find<CreateTodoUseCase>()
+
         
         ),
     );
