28aeb3327635d1d72d95f10c5afba6fe6c2c8c49
Ade Ripaldi <ripaldinuralim@gmail.com>
2026-07-28 10:50:18 +0700

    sStep 1: Buat Entity Post

---
+++ lib/features/todos/domain/entities/post_entity.dart
@@ -0,0 +1,13 @@
+class PostEntity {
+    const PostEntity({
+    required this.id,
+    required this.userId,
+    required this.title,
+    required this.body,
+  });
+
+  final int id;
+  final int userId;
+  final String title;
+  final String body;
+}
