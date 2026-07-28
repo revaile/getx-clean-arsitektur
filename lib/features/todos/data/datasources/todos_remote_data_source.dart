50b96223bfbbaef93fe2a07a3cbf7e43979aff63
Ade Ripaldi <ripaldinuralim@gmail.com>
2026-07-28 15:07:57 +0700

    Step 1 (id):  Tambahkan Method di Remote Data Source

--- lib/features/todos/data/datasources/todos_remote_data_source.dart
+++ lib/features/todos/data/datasources/todos_remote_data_source.dart
@@ -3,13 +3,18 @@ import '../../../../core/network/api_client.dart';
 import '../models/todo_model.dart';
 
 abstract class TodosRemoteDataSource {
+  // abstrak buat list
   Future<List<TodoModel>> getTodos();
+  // abstrak buat get by id
+  Future<TodoModel> getTodo(int id);
 }
 
 class TodosRemoteDataSourceImpl implements TodosRemoteDataSource {
   const TodosRemoteDataSourceImpl(this._apiClient);
 
   final ApiClient _apiClient;
+
+  //untuk list
 
   @override
   Future<List<TodoModel>> getTodos() async {
@@ -24,4 +29,12 @@ class TodosRemoteDataSourceImpl implements TodosRemoteDataSource {
         .map(TodoModel.fromJson)
         .toList();
   }
+
+  //untuk get by id
+    @override
+    Future<TodoModel> getTodo(int id) async {
+    final response = await _apiClient.get('${ApiConstants.todos}/$id');
+    return TodoModel.fromJson(response as Map<String, dynamic>);
+  }
+
 }
