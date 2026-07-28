01cd989f3e352735eda50750dd6f886b3d3795dd
Ade Ripaldi <ripaldinuralim@gmail.com>
2026-07-28 13:40:10 +0700

    Step 7: Buat Controller

---
+++ lib/features/todos/presentation/controllers/todos_controller.dart
@@ -0,0 +1,36 @@
+import 'package:get/get.dart';
+import 'package:getx_clean_arsitektur/features/todos/domain/entities/todo_entity.dart';
+import 'package:getx_clean_arsitektur/features/todos/domain/usecases/get_todos_usecase.dart';
+
+
+class TodosController extends GetxController {
+  TodosController(
+    this._getTodosUseCase,
+  );
+
+  final GetTodosUseCase _getTodosUseCase;
+
+  final todos = <TodoEntity>[].obs;
+  final errorMessage = ''.obs;
+  final isLoading = false.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    getTodos();
+  }
+
+  Future<void> getTodos() async {
+    isLoading.value = true;
+    errorMessage.value = '';
+
+    final result = await _getTodosUseCase();
+
+    result.fold(
+      onFailure: (message) => errorMessage.value = message,
+      onSuccess: (data) => todos.assignAll(data),
+    );
+
+    isLoading.value = false;
+  }
+}
