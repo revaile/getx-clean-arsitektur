9bc444acdf71defae26036ad1c42ad6589b8be82
Ade Ripaldi <ripaldinuralim@gmail.com>
2026-07-28 10:55:07 +0700

    Step 2: Buat Model Post

---
+++ lib/features/posts/data/models/post_model.dart
@@ -0,0 +1,28 @@
+
+import 'package:getx_clean_arsitektur/features/todos/domain/entities/post_entity.dart';
+
+class PostModel extends PostEntity {
+  const PostModel({
+    required super.userId,
+    required super.id,
+    required super.title,
+    required super.body,
+
+  });
+
+  factory PostModel.fromJson(Map<String, dynamic> json) {
+    return PostModel(
+      userId: json['userId'] as int? ?? 0,
+      id: json['id'] as int? ?? 0,
+      title: json['title'] as String? ?? '-',
+      body: json['body'] as String? ?? '-'
+
+    );
+  }
+
+  // Map<String, dynamic> toJson() {
+  //   return {
+
+  //   };
+  // }
+}
